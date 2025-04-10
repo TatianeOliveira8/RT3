76f3988af4409b79b651193aac15b08b3efe9a83
Tatiane Oliveira <164065945+TatianeOliveira8@users.noreply.github.com>
2025-04-09 21:30:14 -0300

    Operações básicas, modulo

---
+++ src/teste.ts
@@ -0,0 +1,23 @@
+import { Somador, Subtrador, Multiplicador, Divisor } from 'modulort3';
+
+const a = 8;
+const b = 2;
+
+console.log("Soma:");
+console.log(new Somador().calcular(a, b));
+
+console.log("Subtração:");
+console.log(new Subtrador().calcular(a, b));
+
+console.log("Multiplicação:");
+console.log(new Multiplicador().calcular(a, b));
+
+console.log("Divisão:");
+try {
+  console.log(new Divisor().calcular(a, b));
+  console.log("Divisão por zero:");
+  console.log(new Divisor().calcular(a, 0));
+} catch (err: any) {
+  console.log("Erro na divisão:");
+  console.log(err.message || err);
+}
